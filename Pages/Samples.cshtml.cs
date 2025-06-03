3bd1524edd586250e28af86dc2c358465cc216e3
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-06-03 16:21:18 +0330

    samples

---
+++ Pages/Samples.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GanjoorRandomPoemService.Pages
+{
+    public class SamplesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
