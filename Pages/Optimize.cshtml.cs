c02b1b2adb1af1c08f8eb86ab1b3bfea34e0fbe4
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-06-03 09:41:23 +0330

    optimize page

---
+++ Pages/Optimize.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GanjoorRandomPoemService.Pages
+{
+    public class OptimizeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
